0699d58c6994107cc703e8cc89bc77be54d828be
Snehanco <62458185+Snehanco@users.noreply.github.com>
2022-08-11 01:01:59 +0530

    message

---
+++ db/config.js
@@ -0,0 +1,10 @@
+const mongoose= require('mongoose');
+require("dotenv").config()
+
+
+const DB= process.env.DATABASE;
+
+mongoose.connect(DB,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(()=>console.log('Connected to database')).catch((error)=>console.log(error.message));
